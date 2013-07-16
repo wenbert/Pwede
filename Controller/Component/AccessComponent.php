7a4aa0e8b8bf24bf3d98411822cbb04321be1bd2
Wenbert Del Rosario <Wenbert Del Rosario>
2013-07-16 13:30:30 +1200

    Debugs for AccessComponent

--- Controller/Component/AccessComponent.php
+++ Controller/Component/AccessComponent.php
@@ -6,10 +6,15 @@ class AccessComponent extends Component {
      * Settings array
      * group_model  The classname of the Group (defaults to "Group")
      * group_model_fk  The foreign key (defaults to "group_id")
+     * auth_group_id    The Group ID of the currently logged in user
      * 
      * @var array
      */
-    var $settings = array();
+    var $settings = array(
+        'group_model' => 'Group',
+        'group_model_fk' => 'group_id',
+        'auth_group_id' => 'group_id'
+    );
 
     public function __construct(ComponentCollection $collection, $settings = array()) {
         $this->settings = $settings;
@@ -19,7 +24,7 @@ class AccessComponent extends Component {
         }
 
         if(!isset($this->settings['group_model_fk'])) {
-            $this->settings['group_model'] = 'group_id';
+            $this->settings['group_model_fk'] = 'group_id';
         }
     }
 
@@ -51,7 +56,26 @@ class AccessComponent extends Component {
             ))
         );
 
-        $this->GroupsPwederesource->recursive = 0;
-        debug($this->GroupsPwederesource->find('all'));
+        // debug(AuthComponent::user());
+
+        //I'm not sure if this is the best way to do this.
+        //We are assuming that a "group id" exists in the Auth object
+        //of the currently logged in user.
+        //For now, we are restricted to have ONE GROUP per user.
+        $gid = AuthComponent::user($this->settings['auth_group_id']);
+
+        //After login, fetch all 
+        $resources = $this->GroupsPwederesource->findAllByGroupId(AuthComponent::user('group_id'));
+        // debug($this->request->params);
+        // debug($resources);
+        foreach($resources AS $key => $resource) {
+            if (
+                $resource['Pwederesources']['plugin'] === $this->request->params['plugin'] &&
+                $resource['Pwederesources']['controller'] === $this->request->params['controller'] &&
+                $resource['Pwederesources']['action'] === "*"
+            ) {
+                //allow all actions for plugin-controller combination
+            }
+        }
      }
 }

c3538802fdd83e78dd96e40b7c22cf08271f9132
Wenbert Del Rosario <Wenbert Del Rosario>
2013-07-19 11:17:49 +1200

    added bootstrap file for cache configs

---
+++ Config/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+    Cache::config('short', array(
+        'engine' => 'File',
+        'duration' => '+1 hours',
+        'path' => CACHE,
+        'prefix' => 'pwede_short_'
+    ));
+
+    // long
+    Cache::config('long', array(
+        'engine' => 'File',
+        'duration' => '+1 week',
+        'probability' => 100,
+        'path' => CACHE . 'long' . DS,
+        'prefix' => 'pwede_long_'
+    ));

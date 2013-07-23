c841eaab45e0c29ce41d1b1663024dbcb0c56ec3
Wenbert Del Rosario <Wenbert Del Rosario>
2013-07-23 17:37:49 +1200

    Main App Controller

--- Controller/PwedeAppController.php
+++ Controller/PwedeAppController.php
@@ -1,8 +1,8 @@
 <?php
-App::uses('AppController', 'Controller');
-// App::uses('GridAppController', 'Grid.Controller');
-// class PwedeAppController extends GridAppController {
-class PwedeAppController extends AppController {
+// App::uses('AppController', 'Controller');
+// class PwedeAppController extends AppController {
+App::uses('GridAppController', 'Grid.Controller');
+class PwedeAppController extends GridAppController {
     
     public function beforeFilter() {
         parent::beforeFilter();

4b11946d391c24d896a6e826951512492c8b3dcf
Wenbert Del Rosario <Wenbert Del Rosario>
2013-07-25 14:39:37 +1200

    Pwede/Dashboards should always be in "Resource"
    Pwede/Dashboards will be the "default" landing page for logged in users

--- Config/Schema/sql/pwede.sql
+++ Config/Schema/sql/pwede.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 19, 2013 at 03:57 AM
+-- Generation Time: Jul 25, 2013 at 02:38 AM
 -- Server version: 5.5.20
 -- PHP Version: 5.3.10
 
@@ -17,43 +17,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `groups_pwederesources`
---
-
-CREATE TABLE IF NOT EXISTS `groups_pwederesources` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `group_id` int(11) NOT NULL,
-  `pwederesource_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;
-
---
--- Dumping data for table `groups_pwederesources`
---
-
-INSERT INTO `groups_pwederesources` (`id`, `group_id`, `pwederesource_id`) VALUES
-(1, 1, 1);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `pwederesources`
 --
 
 CREATE TABLE IF NOT EXISTS `pwederesources` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `plugin` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `controller` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `action` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `plugin` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `controller` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `action` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `named` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `pass` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `query` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `pwederesources`
 --
 
 INSERT INTO `pwederesources` (`id`, `plugin`, `controller`, `action`, `named`, `pass`, `query`) VALUES
-(1, '*', '*', NULL, NULL, NULL, NULL);
+(1, '*', '*', '', NULL, NULL, NULL),
+(2, 'pwede', 'dashboards', '*', NULL, NULL, NULL);

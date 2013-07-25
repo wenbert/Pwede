fd5325623199db747861512a9608c06a0ff3925e
Wenbert Del Rosario <Wenbert Del Rosario>
2013-07-25 13:59:26 +1200

    Resource validation

--- Model/Pwederesource.php
+++ Model/Pwederesource.php
@@ -12,34 +12,67 @@ class Pwederesource extends PwedeAppModel {
  *
  * @var string
  */
-	public $displayField = 'id';
+    // public $displayField = 'id';
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+    public $validate = array(
+        'plugin' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'controller' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        )
+        'action' => array(
+            'notempty' => array(
+                'rule' => array('notempty'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        )
+    );
+
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
 
 /**
  * hasAndBelongsToMany associations
  *
  * @var array
  */
-	// This is bound on the fly in AccessComponent.
+    // This is bound on the fly in AccessComponent.
 
-	// public $hasAndBelongsToMany = array(
-	// 	'Group' => array(
-	// 		'className' => Configure::read('Pwede.GroupModel'),
-	// 		// 'className' => 'UserManager.Group',
-	// 		'joinTable' => 'groups_pwederesources',
-	// 		'foreignKey' => 'pwederesource_id',
-	// 		'associationForeignKey' => 'group_id',
-	// 		'unique' => 'keepExisting',
-	// 		'conditions' => '',
-	// 		'fields' => '',
-	// 		'order' => '',
-	// 		'limit' => '',
-	// 		'offset' => '',
-	// 		'finderQuery' => '',
-	// 		'deleteQuery' => '',
-	// 		'insertQuery' => ''
-	// 	)
-	// );
+    // public $hasAndBelongsToMany = array(
+    //  'Group' => array(
+    //      'className' => Configure::read('Pwede.GroupModel'),
+    //      // 'className' => 'UserManager.Group',
+    //      'joinTable' => 'groups_pwederesources',
+    //      'foreignKey' => 'pwederesource_id',
+    //      'associationForeignKey' => 'group_id',
+    //      'unique' => 'keepExisting',
+    //      'conditions' => '',
+    //      'fields' => '',
+    //      'order' => '',
+    //      'limit' => '',
+    //      'offset' => '',
+    //      'finderQuery' => '',
+    //      'deleteQuery' => '',
+    //      'insertQuery' => ''
+    //  )
+    // );
 
 }
